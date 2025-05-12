3432e3da19df543e0d825f2ddf025783e4c78f62
ztNozdormu <w1999wtw3537@sina.com>
2025-05-12 13:40:07 +0800

    log routes update

--- src/server/routes.rs
+++ src/server/routes.rs
@@ -14,26 +14,34 @@ pub fn routes(
     let version = api.and(warp::path("version")).map(handlers::version);
     let sysinfo = api.and(warp::path("sysinfo")).map(handlers::sysinfo);
     let health = api.and(warp::path("health")).map(handlers::health);
-
+    let logs_sse = api.and(warp::path("logs").and(warp::path("sse") // 实时 SSE 接口
+        .and(warp::get())
+        .and(with_tx(state.tx.clone()))
+        )).and_then(sse_logs);
+    let logs = api.and(warp::path("logs").and(warp::get() // 历史查询接口
+                                                  .and(warp::query::<LogQuery>())
+                                                  .and(with_cache(state.cache)))).and_then(query_logs);
     // 日志服务路由
-    let log_routes = warp::path("logs")
-        .and(
-            warp::path("sse") // 实时 SSE 接口
-                .and(warp::get())
-                .and(with_tx(state.tx.clone()))
-                .and_then(sse_logs),
-        )
-        .or(warp::get() // 历史查询接口
-            .and(warp::query::<LogQuery>())
-            .and(with_cache(state.cache))
-            .and_then(query_logs));
+    // let log_routes = api.and(warp::path("logs")
+    //     .and(
+    //         warp::path("sse") // 实时 SSE 接口
+    //             .and(warp::get())
+    //             .and(with_tx(state.tx.clone()))
+    //             .and_then(sse_logs),
+    //     )
+    //     .or(warp::get() // 历史查询接口
+    //         .and(warp::query::<LogQuery>())
+    //         .and(with_cache(state.cache))
+    //         .and_then(query_logs)));
 
     // TODO robot execution routes
 
     warp::path::end()
         .map(handlers::index)
         .or(ping)
-        .or(log_routes)
+        .or(logs_sse)
+        .or(logs)
+        // .or(log_routes)
         .or(version)
         .or(sysinfo)
         .or(health)
