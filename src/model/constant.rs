02bf2d1789b2a0bb95778c32c6ae301e77b9bd0b
ztNozdormu <w1999wtw3537@sina.com>
2025-06-10 11:31:17 +0800

    remove trade aggregation constants

--- src/model/constant.rs
+++ src/model/constant.rs
@@ -1,4 +1,32 @@
-pub use trade_aggregation::{D1, H1, H12, H2, H4, H8, M1, M15, M30, M5};
+/// 1 Minute candle period
+pub const M1: i64 = 60;
+
+/// 5 Minute candle period
+pub const M5: i64 = 300;
+
+/// 15 Minute candle period
+pub const M15: i64 = 900;
+
+/// 30 Minute candle period
+pub const M30: i64 = 1800;
+
+/// 1 Hour candle period
+pub const H1: i64 = 3600;
+
+/// 2 Hour candle period
+pub const H2: i64 = 7200;
+
+/// 4 Hour candle period
+pub const H4: i64 = 14400;
+
+/// 8 Hour candle period
+pub const H8: i64 = 28800;
+
+/// 12 Hour candle period
+pub const H12: i64 = 43200;
+
+/// 1 Day candle period
+pub const D1: i64 = 86400;
 
 /// 3 Minute candle period
 pub const M3: i64 = 180;
